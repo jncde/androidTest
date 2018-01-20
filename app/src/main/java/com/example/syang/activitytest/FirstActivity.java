fd6ec83168dcbfaeab43d847cd476ba588d25032
Shu Yang <Shu.Yang@inform-software.com>
2018-01-20 13:14:59 +0100

    inplicit intent

--- app/src/main/java/com/example/syang/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/syang/activitytest/FirstActivity.java
@@ -25,8 +25,11 @@ public class FirstActivity extends AppCompatActivity {
 //                        .show();
 //                finish();
 
-                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+//                startActivity(intent);
+                Intent intent = new Intent("com.example.activitytest.ACTION_START");
                 startActivity(intent);
+
             }
         });
     }
